be7f9cfbc777a515e7ad97eef29eb6ce016174e6
Pasindu Mendis <pasindu.m@intervest.lk>
2024-12-11 16:17:18 +0530

    change menu

--- components/organism/menu/menu.tsx
+++ components/organism/menu/menu.tsx
@@ -9,12 +9,12 @@ const Menu = () => {
       </div>
       <div className="grid lg:grid-cols-1 grid-cols-1 gap-4">
         <div className="col-span-1">
-          <div className="image-2 w-[80vw] mx-auto">
+          <div className="image-2 w-[100%] mx-auto px-[20px]">
             <img alt="img-2" src="/gallery/menu/food.jpg" />
           </div>
         </div>
         <div className="col-span-1">
-          <div className="image-2 w-[80vw] mx-auto">
+          <div className="image-2 w-[100%] mx-auto px-[20px]">
             <img alt="img-2" src="/gallery/menu/drinks.jpg" />
           </div>
         </div>
