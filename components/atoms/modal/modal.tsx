7b77b36a8aae5c94a27c4fc5e4e92bd8282207ce
Pasindu Mendis <pasindu.m@intervest.lk>
2024-12-20 19:37:28 +0530

    update gallery and add carousal

--- components/atoms/modal/modal.tsx
+++ components/atoms/modal/modal.tsx
@@ -1,27 +1,67 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const Modal = ({
   children,
   onClose,
+  className,
 }: Readonly<{
   children: React.ReactNode;
-  onClose: () => void;
+  onClose?: () => void;
+  className?: string;
 }>) => {
+  const modalRef = useRef<HTMLDivElement | null>(null);
+
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      onClose && onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
   return (
-    <div
-      className="fixed left-0 top-0 z-[1000] bg-overlay-color w-[100vw] h-[100vh]"
-      onClick={() => onClose()}
-    >
+    <div className="fixed left-0 top-0 z-[1000] bg-overlay-color w-[100vw] h-[100vh]">
+      <button
+        style={{
+          position: "fixed",
+          top: "30px",
+          right: "30px",
+        }}
+        onClick={() => onClose && onClose()}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="close-icon"
+          width="24"
+          height="24"
+        >
+          <line x1="18" y1="6" x2="6" y2="18" />
+          <line x1="6" y1="6" x2="18" y2="18" />
+        </svg>
+      </button>
       <div
-        className="bg-black"
+        className={className}
         style={{
           background: "white",
-          position: "fixed",
+          position: "absolute",
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
           borderRadius: 8,
         }}
+        ref={modalRef}
+        role="button"
+        tabIndex={0}
       >
         {children}
       </div>
--- components/organism/gallery/gallery.tsx
+++ components/organism/gallery/gallery.tsx
@@ -1,10 +1,14 @@
+import Modal from "@/components/atoms/modal/modal";
 import GalleryLogo from "@/components/molecules/logo/gallery-logo/gallery-logo";
 import useMediaQuery from "@/hooks/use-media-query";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const Gallery = () => {
   const isDesktop = useMediaQuery("(min-width: 904px)");
+  const [isModal, setIsModal] = useState(false);
   return (
     <div className="lg:max-w-[1200px] max-w-[100vw] mx-auto p-[30px] mt-[50px]">
       {!isDesktop && (
@@ -40,7 +44,7 @@ const Gallery = () => {
           <div className="image-2 w-[100%] relative  h-[512px]">
             <Image
               alt="img-2"
-              src="/gallery/gallery3.JPG"
+              src="/gallery/gallery3.jpg"
               layout="fill"
               objectFit="cover"
             />
@@ -63,16 +67,129 @@ const Gallery = () => {
               objectFit="cover"
             />
           </div>
-          <div className="image-2 w-[100%] relative   h-[512px] mt-[16px]">
+          <div className="image-2 w-[100%] relative h-[512px] mt-[16px]">
+            {/* Image */}
             <Image
               alt="img-2"
               src="/gallery/gallery6.jpg"
               layout="fill"
               objectFit="cover"
             />
+
+            {/* Overlay */}
+            <div
+              role="button"
+              tabIndex={0}
+              className="absolute inset-0 bg-black bg-opacity-50 hover:bg-opacity-80"
+              onClick={() => setIsModal(true)}
+            >
+              <div className="flex items-center justify-center h-full">
+                <div className="font-[600] text-[36px]">More...</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
+      {isModal && (
+        <Modal
+          className="!bg-transparent border-spacing-2 border-[4px]"
+          onClose={() => setIsModal(false)}
+        >
+          <div>
+            <div className="image-2 w-[80vw] max-w-[750px]">
+              <Carousel
+                className="max-h-[512px]"
+                selectedItem={5}
+                emulateTouch
+                useKeyboardArrows
+                showStatus={false}
+              >
+                <div className="image-2 w-[100%] relative h-full">
+                  <Image
+                    alt="img-2"
+                    src="/gallery/gallery1.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+
+                <div className="image-2 w-[100%] relative h-[512px]">
+                  <Image
+                    alt="img-2"
+                    src="/gallery/gallery2.JPG"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+
+                <div className="image-2 w-[100%] relative  h-full">
+                  <Image
+                    alt="img-2"
+                    src="/gallery/gallery3.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+
+                <div className="image-2 w-[100%] relative h-[512px]">
+                  <Image
+                    alt="img-2"
+                    src="/gallery/gallery4.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+
+                <div className="image-2 w-[100%] relative  h-full">
+                  <Image
+                    alt="img-2"
+                    src="/gallery/gallery5.JPG"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+
+                <div className="image-2 w-[100%] relative h-[512px]">
+                  <Image
+                    alt="img-2"
+                    src="/gallery/gallery6.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+
+                <div className="image-2 w-[100%] relative  h-full">
+                  <Image
+                    alt="img-2"
+                    src="/gallery/gallery7.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+
+                <div className="image-2 w-[100%] relative h-[512px]">
+                  <Image
+                    alt="img-2"
+                    src="/gallery/gallery8.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+
+                <div className="image-2 w-[100%] relative  h-full">
+                  <Image
+                    alt="img-2"
+                    src="/gallery/gallery9.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+              </Carousel>
+            </div>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
